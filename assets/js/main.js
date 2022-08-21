da401f59560762781a6f3ea4e8a1abc1e98e07a2
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-08-20 17:09:14 -0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+jQuery(function ($) {
+    $('.clientsOpinionSlider').owlCarousel({
+        rtl: true, autoplay: false,
+        loop: false,
+        nav: true,
+        dots: false,
+        items: 1,
+        navText: ['<i class="fa-solid fa-chevron-right"></i>','<i class="fa-solid fa-chevron-left"></i>']
+    });
+});
+
