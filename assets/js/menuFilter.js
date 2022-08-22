c05c7bbc547a3b43f3b8e5ea9e9d7aa19f5ec97a
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-08-22 16:41:57 -0700

    upload

--- assets/js/menuFilter.js
+++ assets/js/menuFilter.js
@@ -1,57 +1,57 @@
-jQuery(function () {
-    var filter_container = jQuery('#menuFilter');
-    filter_container.children().css('position','relative');	
+jQuery(function ($) {
+
+    var filter_container = $('#menuFilter');
+
+    filter_container.children().css('position', 'relative');
+    filter_container.masonry({
+        singleMode: true,
+        itemSelector: '.menuFilterItem:not(.hide)',
+        animate: true,
+        animationOptions: { duration: 800, queue: false }
+    });
+    $(window).resize(function () {
+        var temp_width = filter_container.children().filter(':first')();
         filter_container.masonry({
+            columnWidth: temp_width,
             singleMode: true,
             itemSelector: '.menuFilterItem:not(.hide)',
             animate: true,
             animationOptions: { duration: 800, queue: false }
         });
-        jQuery(window).resize(function () {
-             temp_width = filter_container.children().filter(':first')();
-            filter_container.masonry({
-                columnWidth: temp_width,
-                singleMode: true,
-                itemSelector: '.menuFilterItem:not(.hide)',
-                animate: true,
-                animationOptions: { duration: 800, queue: false }
-            });
-        });
-        jQuery('ul#menuTabs a').click( function (e) {
+    });
+    $('ul#menuTabs a').on('click', function (e) {
 
-            jQuery(this).addClass("active");
-            jQuery(this).parents("li").siblings().children("a").removeClass("active");
-            e.preventDefault();
+        $(this).addClass("active");
+        $(this).parents("li").siblings().children("a").removeClass("active");
+        e.preventDefault();
 
-           var  select_filter = jQuery(this).attr('data-value');
+        var select_filter = $(this).attr('data-value');
 
-            if (select_filter == "All" || jQuery(this).parent().index() == 0) {
+        if (select_filter == "All" || $(this).parent().index() == 0) {
             filter_container.children().each(function () {
-               
-                    if ($(this).hasClass('hide')) {
-                        $(this).removeClass('hide');
-                        $(this).fadeIn();
-                    }
-                });
-            } else {
-                filter_container.children().not('.' + select_filter).each(function () {
-                    if (!$(this).hasClass('hide')) {
-                        $(this).addClass('hide');
-                        $(this).fadeOut();
-                    }
-                });
-                filter_container.children('.' + select_filter).each(function () {
-                    if ($(this).hasClass('hide')) {
-                        $(this).removeClass('hide');
-                        $(this).fadeIn();
-                    }
-                });
-            }
+                if ($(this).hasClass('hide')) {
+                    $(this).removeClass('hide');
+                    $(this).fadeIn();
+                }
+            });
+        } else {
+            filter_container.children().not('.' + select_filter).each(function () {
+                if (!$(this).hasClass('hide')) {
+                    $(this).addClass('hide');
+                    $(this).fadeOut();
+                }
+            });
+            filter_container.children('.' + select_filter).each(function () {
+                if ($(this).hasClass('hide')) {
+                    $(this).removeClass('hide');
+                    $(this).fadeIn();
+                }
+            });
+        }
 
-            filter_container.masonry();
+        filter_container.masonry();
 
-        });
-
+    });
 
 
 });
